58f48392169c3e44080530beacfa1ea4bbf212f6
VanadiumJade <36226399+VanadiumJade@users.noreply.github.com>
2019-01-16 08:38:33 +1300

    config in json

--- UnifiVideo.py
+++ UnifiVideo.py
@@ -1,55 +1,73 @@
 # mqtt References
 import time
 import paho.mqtt.client as mqtt
-import time
 
 # api references
 import requests
 import json
 
+#defaults
+FailureRestart = "10"
+
 def on_message(client, userdata, message):
-        #print("message topic=",message.topic + " - " + str(message.payload.decode("utf-8")))
-        for L1 in Config['MQTT_Subscribe']:
-                for L2 in L1['Rec']:
-                        MQTT_Subscribe = L2['MQTT']
-                        if message.topic == MQTT_Subscribe:
-                                print("matched " + message.topic )
-                                for L3 in L2['data']:
-                                        CameraTag = L3['CameraTag']
-                                        CameraID = L3['CameraID']
-                                        CameraName = L3['CameraName']
-
-                                        s = requests.Session()
-                                        response = requests.get("http://xxx:7080/api/2.0/camera/")
-
-                                        url = "http://xxx:7080/api/2.0/camera/" + CameraID + "?apiKey=xxx"
-                                        MessageString = CameraTag + " (" + str(message.payload.decode("utf-8")) + "°c)"
-                                        data = { "name": CameraName, "osdSettings": { "tag": MessageString } }
-                                        headers = {"Content-Type" : "application/json"}
-
-                                        response = requests.put(url, data=json.dumps(data), headers=headers )
-                                        MessageSent = 'request: ' + str(response.status_code) + ', CameraID: ' + CameraID + ', Message:' + MessageString
-                                        print(MessageSent)
-
+	for L1 in Config['MQTT_Subscribe']:
+		for L2 in L1['Rec']:
+			MQTT_Subscribe = L2['MQTT']
+			if message.topic == MQTT_Subscribe:
+				print("matched " + message.topic )
+				for L3 in L2['data']:
+					CameraTag = L3['CameraTag']
+					CameraID = L3['CameraID']
+					CameraName = L3['CameraName']
+					CameraTagPrefix = L3['CameraTagPrefix']
+					CameraTagPostfix = L3['CameraTagPostfix']
+					
+					s = requests.Session()
+					url = "http://" + Host + "/api/2.0/camera/" + CameraID + "?apiKey=" + API_Key
+					MessageString = CameraTag + CameraTagPrefix + str(message.payload.decode("utf-8")) + CameraTagPostfix
+					data = { "name": CameraName, "osdSettings": { "tag": MessageString } }
+					headers = {"Content-Type" : "application/json"}
+					
+					response = requests.put(url, data=json.dumps(data), headers=headers )
+					MessageSent = 'request: ' + str(response.status_code) + ', CameraID: ' + CameraID + ', Message:' + MessageString
+					print(MessageSent)
 
 while True:
-        try:
-                print("Starting")
-                with open('/etc/UnifiCamera.json','r') as ConfigFile:
-                        Config = json.load(ConfigFile)
-                broker_address = "xx.xx.xx.xx"
-                print("creating new instance")
-                client = mqtt.Client("UnifiCamera") #create new instance
-                client.on_message=on_message #attach function to callback
-                print("connecting to broker")
-                client.connect(broker_address) #connect to broker
-                client.loop_start() #start the loop
-                #SubString = "openhab/in/Bedroom2Heater/Temp"
-                SubString = "openhab/in/#"
-                print("Subscribing to topic",SubString)
-                #client.subscribe("openhab/in/#")
-                client.subscribe(SubString)
-                time.sleep(3600) # wait
-                client.loop_stop() #stop the loop
-        except:
-                time.sleep(10) # wait
+	try:
+		print("........................")
+		print("Starting")
+		with open('UnifiCamera.json','r') as ConfigFile:
+			Config = json.load(ConfigFile)
+
+		ConfigMain = Config['Config']
+		print("........................")
+		API_Key = ConfigMain[0]["API_Key"]
+		Host = ConfigMain[0]["Host"]
+		FailureRestart = ConfigMain[0]["FailureRestart"]
+		MQTT_ForceReconnect = ConfigMain[0]["MQTT_ForceReconnect"]
+
+		#Broker Config
+		MQTT_Broker 		= ConfigMain[0]["MQTT_Broker"]
+		MQTT_Name 			= ConfigMain[0]["MQTT_Name"]
+		MQTT_Subscription 	= ConfigMain[0]["MQTT_Subscription"]
+
+		print("FailureRestart: " + FailureRestart)
+		print("MQTT_ForceReconnect: " + MQTT_ForceReconnect)
+		print("MQTT_Broker: " + MQTT_Broker)
+		print("MQTT_Name: " + MQTT_Name)
+		print("MQTT_Subscription: " + MQTT_Subscription)
+		print("API_Key: " + API_Key)
+		print("........................")
+
+		print("creating new instance")
+		client = mqtt.Client(MQTT_Name) #create new instance
+		client.on_message=on_message #attach function to callback
+		print("connecting to broker " + MQTT_Broker + " with subscription=" + MQTT_Subscription)
+		client.connect(MQTT_Broker) #connect to broker
+		client.loop_start() #start the loop
+		print("Subscribing to topic",MQTT_Subscription)
+		client.subscribe(MQTT_Subscription)
+		time.sleep(int(MQTT_ForceReconnect)) # wait
+		client.loop_stop() #stop the loop
+	except:
+		time.sleep(FailureRestart) # wait
